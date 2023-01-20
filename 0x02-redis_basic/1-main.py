0ef6de54d0ef4471809dfac758255ddba443b604
Lema Kefyalew <50364939+lemakefyalew@users.noreply.github.com>
2023-01-20 10:28:54 +0300

    redis_basic

---
+++ 0x02-redis_basic/1-main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Main file """
+
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("second")
+print(s2)
+s3 = cache.store("third")
+print(s3)
+s4 = cache.store(3)
+print(s4)
+s5 = cache.store(3.14)
+print(s5)
+
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
+
+""" Expected output
+-----------------
+bob@dylan:~$ ./main.py
+04f8dcaa-d354-4221-87f3-4923393a25ad
+a160a8a8-06dc-4934-8e95-df0cb839644b
+15a8fd87-1f55-4059-86aa-9d1a0d4f2aea
+inputs: [b"('first',)", b"('secont',)", b"('third',)"]
+outputs: [b'04f8dcaa-d354-4221-87f3-4923393a25ad', b'a160a8a8-06dc-4934-8e95-df0cb839644b', b'15a8fd87-1f55-4059-86aa-9d1a0d4f2aea']
+bob@dylan:~$
+
+"""
